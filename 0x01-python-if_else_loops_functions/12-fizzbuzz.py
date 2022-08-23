4964d9689654ed9762326e59c028621f676edd3b
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-08-24 00:56:40 +0300

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    i = 1
+    while i <= 100:
+        if i % 3 == 0:
+            print('Fizz')
+            continue
+        elif i % 5 == 0:
+            print('Buzz')
+            continue
+        elif i % 15 == 0:
+            print('FizzBuzz')
+            continue
+        else:
+            print("{:d}".format(i), end="")
+    i += 1
