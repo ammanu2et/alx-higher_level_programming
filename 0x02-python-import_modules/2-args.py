7425189d7fbf6ddb938fced8b2f1bdb2b90b9106
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-08-25 20:46:46 +0300

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+i = 0
+if argv[i] < 1:
+    print("0 arguments.")
+
+if argv[1]:
+    print("{:d} argument:".format(len(argv)), end="")
+    print("{:d} : {:s}".format(argv[i], sys.argv[i]), end="")
+else:
+    print("{:d} arguments:".format(len(argv)), end="")
+    print("{:d} : {:s}".format(argv[i], sys.argv[i]), end="")
