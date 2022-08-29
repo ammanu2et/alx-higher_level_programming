9be36e5634c61409c9ab24808b39ed6445fc4155
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-08-29 17:30:23 +0300

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
-    for i in my_string:
-        if i == "c":
-            pass
-        elif i == 'C':
-            pass
-        else:
-            new += i
-    print(new)
+    rem = ['C', 'c']
+    new = "".join(i for i in my_string if i not in rem)
+    return (new)
