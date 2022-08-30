5ee389a8ea99e16881b294db6e8a16d0027d860b
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-08-30 23:05:38 +0300

    data structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    bool = []
     for i in my_list:
         if i % 2 == 0:
-            return True
+            bool.append(True)
         else:
-            return False
+            bool.append(False)
+    return bool
