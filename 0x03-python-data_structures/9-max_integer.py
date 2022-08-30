c9bdd4da04c0c69e7657499a0a624cab298e26b5
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-08-30 22:35:41 +0300

    data structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    for i in my_list:
+        if i < 0:
+            return None
+        else:
+            my_list.sort()
+            return my_list[-1]
