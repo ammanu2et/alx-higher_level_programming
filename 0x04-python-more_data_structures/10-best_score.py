5fa9a3deb2ddd8651230c96a7fff57d5800344f6
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-01 00:58:42 +0300

    more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        highest = max(a_dictionary, key=a_dictionary.get)
+        return highest
