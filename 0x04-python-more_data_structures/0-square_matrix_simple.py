ec6236c2e560c72222a0fa5838b4fdb4f21c3fde
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-08-31 23:59:58 +0300

    more data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = matrix.copy()
-    for i in new:
-        return i ** 2
+    new_list = []
+    for row in matrix:
+        new_row = sq_list(row)
+        new_list.append(new_row)
+    return new_list
+def sq_list(x):
+    return [i**2 for i in x]
