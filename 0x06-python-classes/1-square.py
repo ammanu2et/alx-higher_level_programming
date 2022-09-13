9f1d967341302c708ff84105e1efe657e5917d5c
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-13 21:06:30 +0300

    oop

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Square is an empty class that defines a square."""
+
+
+class Square:
+    """Empty class Square that defines a square.
+
+    args size (int):
+        The size of a square is crucial for a square,
+        many things depend of it (area computation, etc.), so you,
+        as class builder, must control the type and value of this attribute.
+
+    Attributes:
+        The size of a square is crucial for a square,
+        many things depend of it (area computation, etc.), so you,
+        as class builder, must control the type and value of this attribute.
+
+    """
+
+    def __init__(self, size=None):
+        self.__size == size
