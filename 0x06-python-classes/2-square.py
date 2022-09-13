db4c7df5d18c2f93d67d81989b38b5f963d77b1d
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-13 22:06:11 +0300

    oop

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 """class square defines a square."""
 
-
 class Square:
-    """Empty class Square that defines a square.
-
-    args size (int):
-        The size of a square is crucial for a square,
-        many things depend of it (area computation, etc.), so you,
-        as class builder, must control the type and value of this attribute.
-
-    """
+    """class Square that defines a square."""
 
     def __init__(self, size=0):
 
-    """ Attributes:
-        The size of a square is crucial for a square,
-        many things depend of it (area computation, etc.), so you,
-        as class builder, must control the type and value of this attribute.
+    """ Args:
+        Size (int): size of the square.
     """
-
         if size < 0:
             raise ValueError('size must be >= 0')
         elif type(size) is not int:
