278f1155e786792b695aabc5c8d8d57f3d980c14
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-14 22:53:54 +0300

    Exceptions

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """int: size
+            Returns:
+            size (int): size of the square
+        """
+        return self.__size
+
+    @property setter
+    def size(self, value):
+        """int: sets value into size
+
+        Args:
+            value (int): size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """initialize area
+            Returns:
+            area (int): area of the square
+        """
+        return self.__size ** 2
