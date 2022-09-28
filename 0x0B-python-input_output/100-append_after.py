8cafd72550c63c4f3ee5f7ec6cf9a306d9eae48f
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-29 01:56:51 +0300

    input output

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module that inserts a line to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    filename: the name of the file
+    search_string: string to search
+    new_string: string to append
+    """
+    n_line = []
+    with open(filename, 'r', encoding="utf-8") as t:
+        for line in t:
+            n_line += [line]
+            if line.find(search_string) != -1:
+                n_line += [new_string]
+
+    with open(filename, 'w', encoding="utf-8") as t:
+        t.write("".join(res_line))
