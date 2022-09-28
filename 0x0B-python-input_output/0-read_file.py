289819a91a62418ac1498013e44e0d6dae0cd4f7
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-28 21:31:19 +0300

    input output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" module that contains a function that reads from a file"""
+
+
 def read_file(filename=""):
     """
     read_file -  mode
     Return: read the file.
     """
-    with open(filename, encoding="utf-8") as read_file:
-        print(read_file.read())
+    with open(filename, 'r', encoding="utf-8") as read_file:
+        print(read_file.read(), end="")
