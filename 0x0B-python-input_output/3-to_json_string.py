f9c5be128d38754ffe16f0a25171acf840052572
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-29 00:05:46 +0300

    input output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" module that returns the JSON representation of an object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    my_obj: object
+    Return: the json representation of an object
+    """
+    return json.dumps(my_obj)
