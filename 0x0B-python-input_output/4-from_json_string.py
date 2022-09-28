2007ebabb0703f714c88824952f1b1c3ce319dea
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-29 00:20:12 +0300

    input output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""module that returns an object represented by json"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    my_str: the string or object
+    Return: the json representation of an object
+    """
+    return json.loads(my_str)
