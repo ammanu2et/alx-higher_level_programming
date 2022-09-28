5259a349cd79f16ecc647727a0ec81bdbcec758d
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-29 01:03:39 +0300

    input output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""module that creates an object from a json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    filename: name of the file
+    Return: an object created from json file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
