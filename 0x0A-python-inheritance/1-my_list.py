c117d486f59af7d94c9ba02f8dc3a15b4b2db72d
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-29 21:03:58 +0300

    input output

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module that prints the sorted list"""
+
+
+class MyList(list):
+
+
+    def print_sorted(self):
+        """
+        self: name of the list
+        Return: sorted list
+        """
+        return sort(list)
