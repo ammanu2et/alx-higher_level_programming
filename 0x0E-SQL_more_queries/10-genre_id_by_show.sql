7e0069d4d2a381974c2512d9d493f54ac106f2f0
Ammanuel <ammantd2@gmail.com>
2022-11-02 20:33:08 +0300

    SQL-more_queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- lists all shows in hbtn_0d_tvshows.
 SELECT s.title, g.genre_id
     FROM tv_shows AS s
-        INNER JOIN tv_shows_genres AS g
+        INNER JOIN tv_show_genres AS g
         ON s.id = g.show_id
 ORDER BY s.title, g.genre_id;
