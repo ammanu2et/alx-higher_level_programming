0226abbf7a1ab5606350d7b66a46948823b6506c
Ammanuel <ammantd2@gmail.com>
2022-11-02 11:07:51 +0300

    SQL

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 --List all records of the second_table.
 --Records are orderd by descending score.
-SELECT score, name FROM second_table
+SELECT score, `name` FROM second_table
 ORDER BY score DESC;
